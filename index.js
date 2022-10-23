aa608883efeaa2e7f24a482fbf796cf9e21d51f3
collins mbathi <perezcoolins@gmail.com>
2022-10-23 21:36:43 +0300

    added some files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+// const db = require("./models/index");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+//database
+// db.sequelize.sync()
+// .then(() => {
+//   console.log("Synced db.");
+// })
+// .catch((err) => {
+//   console.log("Failed to sync db: " + err.message);
+// });
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening to port ${PORT}`);
+});
---
+++ models/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: "mysql",
+    operatorsAliases:0,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      },
+    socketPath: '/var/run/mysqld/mysqld.sock'
+  });
+
+  const db = {}
+ db.Sequelize = Sequelize;
+ db.sequelize = sequelize;
