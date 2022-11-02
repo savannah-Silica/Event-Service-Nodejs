6ebb03d8853080f808a904dec6d7d7beac39cc72
paullaster <paullasterokoth98@gmail.com>
2022-11-02 14:16:31 +0300

    swagger documentation

--- controllers/utils/swagger.js
+++ controllers/utils/swagger.js
@@ -1,3 +1,26 @@
 //Dependencies:
-const express = require ( 'express' );
-const swaggerJsdoc = require ( 'swagger-jsdoc')
+const swaggerJsdoc = require ( 'swagger-jsdoc');
+require ( 'dotenv').config();
+
+//Options
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Event service API',
+            version: '1.0.0',
+            description: ' Savannah Silica Event Service API documentation'
+        },
+        servers: [
+            {
+                url: `${process.env.APP_URL}:${process.env.APP_PORT}`,
+            }
+        ],
+    },
+    apis: ['./routers/*.js']
+};
+
+const specs = swaggerJsdoc (options);
+
+//Export specs:
+module.exports = specs;
